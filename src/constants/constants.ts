104b47ebf98bb58273f3ff71cb551a10cbccc338
amnambiar <anu.sripuri@gmail.com>
2023-08-29 08:29:18 +0530

    Fixed merge error

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,10 +1,13 @@
 export const LocalStorageKeys = {
-    "profile": "profile",
-    "isLoggedIn": "isLoggedIn",
-    "userDetails": "userDetails",
-    "walletName": "walletName",
-    "address": "address",
-    "authToken": "authToken",
-    "accessToken": "accessToken",
-    "hasSubscriptions": "hasSubscriptions"
+    profile: "profile",
+  isLoggedIn: "isLoggedIn",
+  userDetails: "userDetails",
+  walletName: "walletName",
+  address: "address",
+  authToken: "authToken",
+  accessToken: "accessToken",
+  hasSubscriptions: "hasSubscriptions",
+  certificationUuid: "certificationUuid",
+  certificationRunTime: "certificationRunTime",
+  certificationDone: "certificationDone"
 };
