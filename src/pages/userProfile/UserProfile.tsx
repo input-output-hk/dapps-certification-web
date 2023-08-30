7e3f0f0dad6796b2b85fc1230756c8954e8fc656
amnambiar <anu.sripuri@gmail.com>
2023-08-31 01:07:05 +0530

    PLT-6007 - PLT-7262 - UserProfile Save disabled fixes

--- src/pages/userProfile/UserProfile.tsx
+++ src/pages/userProfile/UserProfile.tsx
@@ -15,7 +15,8 @@ import { clearStates,
   clearAccessToken,
   getUserAccessToken,
   verifyRepoAccess,
-  hideConfirmConnection } from "./slices/repositoryAccess.slice";
+  hideConfirmConnection,
+  setRepoAccessible } from "./slices/repositoryAccess.slice";
 import Toast from "components/Toast/Toast";
 import useLocalStorage from "hooks/useLocalStorage";
 import { LocalStorageKeys } from "constants/constants";
@@ -37,6 +38,7 @@ const UserProfile = () => {
   const [canShowConnectModal, setCanShowConnectModal] = useState(false)
   const [focussedOwnerRepo, setFocussedOwnerRepo] = useState(false)
   const [showError, setShowError] = useState("")
+  const [submitting, setSubmitting] = useState(false)
 
   const [searchParams, setSearchParams] = useSearchParams();
   const githubAccessCode = searchParams.get("code");
@@ -55,6 +57,7 @@ const UserProfile = () => {
 
   const initializeFormState = () => {
     form.clearErrors(); // clear form errors
+    clearOwnerRepoError();
     
     const { dapp, contacts, authors, linkedin, twitter, vendor, website } = userDetails;
     let formData: {
@@ -85,7 +88,7 @@ const UserProfile = () => {
       form.reset(formData);
     }
 
-    if (!userDetails.dapp?.owner || !userDetails.dapp?.repo || profileInLS) {
+    if ((address && dapp === null) || profileInLS) {
       setIsEdit(true);
     }
   }
@@ -93,6 +96,7 @@ const UserProfile = () => {
   useEffect(() => {
     if (isEdit) {
       form.setFocus("name"); // focus on first field on Edit mode
+      userDetails?.dapp !== null && dispatch(setRepoAccessible()); // if value is available, repo is accessible
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isEdit]);
@@ -119,6 +123,7 @@ const UserProfile = () => {
     if (isOwnerRepoValidationError()) {
       return;
     }
+    setSubmitting(true)
     const {authors, contacts, linkedin, twitter, vendor, website} = formData
     const submitProfile = async () => {
       const reqData: IUserProfile = {
@@ -147,6 +152,7 @@ const UserProfile = () => {
         );
         setUserDetails(response.payload);
         dispatch(clearAccessToken())
+        setSubmitting(false)
         navigate('/')
       }).catch((errorObj) => {
         let errorMsg = 'Something went wrong. Please try again.'
@@ -235,6 +241,20 @@ const UserProfile = () => {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
+  useEffect(() => {
+    // Repo not accessible
+    if (isEdit) {
+      if (!verifying && !accessible) {
+        form.setError("owner");
+        form.setError("repo");
+      } else {
+        // trigger revalidation of the form
+        form.trigger(["owner", "repo"]);
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [verifying, accessible, form.formState.errors]);
+
   const connectToGithub = async () => {
     const data = {...form.getValues(), owner: owner, repo: repo}
     // store current form data in localStorage
@@ -258,13 +278,11 @@ const UserProfile = () => {
   }
 
   const privateRepoDisclaimer = () => {
-    setTimeout(() => {
-      confirm({
-        title: "Private Repository Access Disclaimer",
-        description: "Auditors need to obtain consent from their customers and acquire the necessary permissions to fork their private Github repositories in order to test the decentralized application (dApp) using the Plutus Testing Tool, created by Input Output Global, Inc (IOG). The Plutus Testing Tool is available on an “AS IS” and “AS AVAILABLE” basis, without any representation or warranties of any kind. IOG is not responsible for the actions, omissions, or accuracy of any third party for any loss or damage of any sort resulting from the forking of repositories and testing of dApps using the Plutus Testing Tool.",
-        confirmationText: "Agree",
-      }).then( connectToGithub )
-    }, 0)
+    confirm({
+      title: "Private Repository Access Disclaimer",
+      description: "Auditors need to obtain consent from their customers and acquire the necessary permissions to fork their private Github repositories in order to test the decentralized application (dApp) using the Plutus Testing Tool, created by Input Output Global, Inc (IOG). The Plutus Testing Tool is available on an “AS IS” and “AS AVAILABLE” basis, without any representation or warranties of any kind. IOG is not responsible for the actions, omissions, or accuracy of any third party for any loss or damage of any sort resulting from the forking of repositories and testing of dApps using the Plutus Testing Tool.",
+      confirmationText: "Agree",
+    }).then( connectToGithub )
   }
 
   const ownerRef = useRef<HTMLInputElement>(null);
@@ -424,13 +442,14 @@ const UserProfile = () => {
                   type="button"
                   buttonLabel={"Edit"}
                   onClick={(e) => {
-                    setIsEdit(!isEdit);
+                    setIsEdit(true);
                   }}
                 />
               ) : (
                 <>
                   <Button
                     type="button"
+                    disabled={submitting}
                     displayStyle="secondary"
                     buttonLabel={"Cancel"}
                     onClick={() => {
@@ -440,9 +459,10 @@ const UserProfile = () => {
                     }}
                   />
                   <Button
-                    disabled={!form.formState.isValid || verifying || (!accessible && canShowConnectModal) || ownerErr || repoErr}
+                    disabled={!form.formState.isValid || verifying || !accessible || ownerErr || repoErr}
                     type="submit"
                     buttonLabel={"Save"}
+                    showLoader={submitting}
                   />
                 </>
               )}
