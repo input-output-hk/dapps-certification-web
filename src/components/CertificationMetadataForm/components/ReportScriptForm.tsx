032c3a848f7cd435ed67dcc295acfa7af44b203d
Mauro Escudero <eehmauro@EehMacBookProM1Max.local>
2023-10-02 23:47:46 -0300

    Resolved some technical debt

--- src/components/CertificationMetadataForm/components/ReportScriptForm.tsx
+++ src/components/CertificationMetadataForm/components/ReportScriptForm.tsx
@@ -1,19 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { Box, Grid, Paper, Typography, TextField, Button } from "@mui/material";
+import { Box, Grid, Typography, Button } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import RemoveIcon from "@mui/icons-material/Remove";
 
 import InputGroup from "compositions/InputGroup";
 import Container from "compositions/InputGroup/components/Container";
+import ReportScriptFormConfirmModal from "./ReportScriptFormConfirmModal";
 import { getScriptFields, getScriptContractFields } from "../utils";
 
-import type { FormState, UseFormRegister, UseFormGetFieldState, FieldArrayWithId, UseFieldArrayAppend, UseFieldArrayRemove } from "react-hook-form";
-import type { ReportForm } from "../interface";
+import type { FormState, UseFormRegister, UseFormGetFieldState, FieldArrayWithId, UseFieldArrayAppend, UseFieldArrayRemove, UseFormWatch } from "react-hook-form";
+import type { ReportForm, ReportFormScript } from "../interface";
 
 import "../index.css";
 
 interface Props {
+  watch: UseFormWatch<ReportForm>;
   formState: FormState<ReportForm>;
   register: UseFormRegister<ReportForm>;
   getFieldState: UseFormGetFieldState<ReportForm>;
@@ -31,90 +33,120 @@ interface InstanceProps {
   onRemove: () => void;
 }
 
-const ReportScriptFormInstance = (props: InstanceProps) => {
-  return (
-    <Box className="p-4 mt-4 rounded-sm border-solid border-gray-inactive">
-      <Box className="flex flex-row items-center">
-        <Typography variant="subtitle2" className="text-main flex-1">
-          DAPP Script (#{props.index+1})
-        </Typography>
-        {props.index > 0 && (
-          <Button
-            variant="outlined" size="small" color="error" className="normal-case"
-            startIcon={<RemoveIcon />} onClick={props.onRemove}
-          >
-            Remove Script
-          </Button>
-        )}
-      </Box>
-      <Box className="mx-[-1em]">
-        <InputGroup
-          fields={getScriptFields(props.index)}
-          formState={props.formState}
-          register={props.register}
-          getFieldState={props.getFieldState}
-        />
-      </Box>
-      <Typography variant="subtitle2" className="text-main mt-2">
-        SmartContract Information
+const ReportScriptFormInstance = (props: InstanceProps) => (
+  <Box className="p-4 mt-4 rounded-sm border-solid border-gray-inactive">
+    <Box className="flex flex-row items-center">
+      <Typography variant="subtitle2" className="text-main flex-1">
+        DAPP Script (#{props.index+1})
       </Typography>
-      <Grid container spacing={2}>
-        <Grid item xs={6}>
-          <Box className="mx-[-1em]">
-            <InputGroup
-              fields={getScriptContractFields(props.index).slice(0,4)}
-              formState={props.formState}
-              register={props.register}
-              getFieldState={props.getFieldState}
-            />
-          </Box>
-        </Grid>
-        <Grid item xs={6}>
-          <Box className="mx-[-1em]">
-            <InputGroup
-              fields={getScriptContractFields(props.index).slice(4,7)}
-              formState={props.formState}
-              register={props.register}
-              getFieldState={props.getFieldState}
-            />
-          </Box>
-        </Grid>
-      </Grid>
+      {props.index > 0 && (
+        <Button
+          variant="outlined" size="small" color="error" className="normal-case"
+          startIcon={<RemoveIcon />} onClick={props.onRemove}
+        >
+          Remove Script
+        </Button>
+      )}
     </Box>
-  );
-}
-
-const ReportScriptForm = (props: Props) => (
-  <Container standalone={props.standalone}>
-    <Typography variant="subtitle1" className="text-main pt-4">
-      DAPP Scripts
-    </Typography>
-    {props.scriptFields.map((field, index) => (
-      <ReportScriptFormInstance
-        key={field.id}
-        index={index}
+    <Box className="mx-[-1em]">
+      <InputGroup
+        fields={getScriptFields(props.index)}
         formState={props.formState}
         register={props.register}
         getFieldState={props.getFieldState}
-        onRemove={() => props.removeScript(index)}
       />
-    ))}
-    <Box className="pt-4 text-right">
-      <Button
-        variant="outlined"
-        size="small"
-        className="button-outlined-main"
-        startIcon={<AddIcon />}
-        onClick={() => props.appendScript({
-          scriptHash: '',
-          contractAddress: '',
-          smartContractInfo: {}
-        })}
-      >
-        Add Script
-      </Button>
     </Box>
-  </Container>
+    <Typography variant="subtitle2" className="text-main mt-2">
+      SmartContract Information
+    </Typography>
+    <Grid container spacing={2}>
+      <Grid item xs={6}>
+        <Box className="mx-[-1em]">
+          <InputGroup
+            fields={getScriptContractFields(props.index).slice(0,4)}
+            formState={props.formState}
+            register={props.register}
+            getFieldState={props.getFieldState}
+          />
+        </Box>
+      </Grid>
+      <Grid item xs={6}>
+        <Box className="mx-[-1em]">
+          <InputGroup
+            fields={getScriptContractFields(props.index).slice(4,7)}
+            formState={props.formState}
+            register={props.register}
+            getFieldState={props.getFieldState}
+          />
+        </Box>
+      </Grid>
+    </Grid>
+  </Box>
 );
+
+const ReportScriptForm = (props: Props) => {
+  const [currentIndex, setCurrentIndex] = useState<number|null>(null);
+
+  const isDirty = (script: ReportFormScript) => {
+    if (script.scriptHash.length > 0) return true;
+    if (script.contractAddress.length > 0) return true;
+    if (Object.keys(script.smartContractInfo).filter(key => (script as any).smartContractInfo[key]?.length > 0 ).length > 0) return true;
+    return false;
+  }
+
+  const onRemove = (index: number) => {
+    if (isDirty(props.watch(`scripts.${index}`))) {
+      setCurrentIndex(index);
+    } else {
+      props.removeScript(index);
+    }
+  }
+
+  const confirmRemove = () => {
+    props.removeScript(currentIndex!);
+    setCurrentIndex(null);
+  }
+
+  return (
+    <>
+      <Container standalone={props.standalone}>
+        <Typography variant="subtitle1" className="text-main pt-4">
+          DAPP Scripts
+        </Typography>
+        {props.scriptFields.map((field, index) => (
+          <ReportScriptFormInstance
+            key={field.id}
+            index={index}
+            formState={props.formState}
+            register={props.register}
+            getFieldState={props.getFieldState}
+            onRemove={() => onRemove(index)}
+          />
+        ))}
+        <Box className="pt-4 text-right">
+          <Button
+            variant="outlined"
+            size="small"
+            className="button-outlined-main"
+            startIcon={<AddIcon />}
+            onClick={() => props.appendScript({
+              scriptHash: '',
+              contractAddress: '',
+              smartContractInfo: {}
+            })}
+          >
+            Add Script
+          </Button>
+        </Box>
+      </Container>
+
+      <ReportScriptFormConfirmModal
+        show={currentIndex !== null}
+        onClose={() => setCurrentIndex(null)}
+        onConfirm={confirmRemove}
+      />
+    </>
+  );
+};
 
 export default ReportScriptForm;
