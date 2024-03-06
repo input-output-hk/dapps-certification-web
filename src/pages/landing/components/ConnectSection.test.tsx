ddd8194c58b3fe25f0460dac317bb9ccc30930b1
KJES4 <Kathrynjeanelizabethstacy@gmail.com>
2024-03-06 10:37:06 -0600

    test: show wallet button
    Add new tests and organize ConnectSection test suite with descibe blocks
    
    PLT-9596

--- src/pages/landing/components/ConnectSection.test.tsx
+++ src/pages/landing/components/ConnectSection.test.tsx
@@ -6,32 +6,44 @@ import '@testing-library/jest-dom';
 import { renderWithProviders } from 'utils/test-utils';
 import ConnectSection from './ConnectSection';
 
-test('open modal when connect button is pressed and show 3 active wallets', async () => {
 
-  window.cardano = {
-    lace: { icon: 'lace', name: 'lace', },
-    nami: { icon: 'nami', name: 'nami', },
-    yoroi: { icon: 'yoroi', name: 'yoroi', },
-  };
+describe('Landing page', () => {
+  test('should render wallet connect button on landing page', () => {
 
-  renderWithProviders(<ConnectSection />);
+    const buttonElement = screen.getByRole('button', { name: /Connect your wallet/i });
 
-  await userEvent.click(screen.getByRole('button', { name: /Connect your wallet/i }));
-
-  expect(screen.getByRole('button', { name: /lace/i })).toBeVisible();
-  expect(screen.getByRole('button', { name: /nami/i })).toBeVisible();
-  expect(screen.getByRole('button', { name: /yoroi/i })).toBeVisible();
-  
+    expect(buttonElement).toBeVisible();
+  });
 });
 
-test('open modal when connect button is pressed and show no active wallets', async () => {
+describe('Landing page modal', () => {
+  test('open modal when connect button is pressed and show 3 active wallets', async () => {
 
-  window.cardano = null;
-
-  renderWithProviders(<ConnectSection />);
-
-  await userEvent.click(screen.getByRole('button', { name: /Connect your wallet/i }));
-
-  expect(screen.getByText('No wallet extensions installed yet!')).toBeVisible();
-
+    window.cardano = {
+      lace: { icon: 'lace', name: 'lace', },
+      nami: { icon: 'nami', name: 'nami', },
+      yoroi: { icon: 'yoroi', name: 'yoroi', },
+    };
+  
+    renderWithProviders(<ConnectSection />);
+  
+    await userEvent.click(screen.getByRole('button', { name: /Connect your wallet/i }));
+  
+    expect(screen.getByRole('button', { name: /lace/i })).toBeVisible();
+    expect(screen.getByRole('button', { name: /nami/i })).toBeVisible();
+    expect(screen.getByRole('button', { name: /yoroi/i })).toBeVisible();
+    
+  });
+  
+  test('open modal when connect button is pressed and show no active wallets', async () => {
+  
+    window.cardano = null;
+  
+    renderWithProviders(<ConnectSection />);
+  
+    await userEvent.click(screen.getByRole('button', { name: /Connect your wallet/i }));
+  
+    expect(screen.getByText('No wallet extensions installed yet!')).toBeVisible();
+  
+  });
 });
