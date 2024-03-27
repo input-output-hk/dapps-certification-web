42fe18934c1f3524c3a98a6523cc8af992c71b25
KJES4 <kathryn.stacy@iohk.io>
2024-03-27 14:15:04 -0500

    PLT-9600 #comment test:refactor for cleaner code

--- src/pages/session/components/AppBar.test.tsx
+++ src/pages/session/components/AppBar.test.tsx
@@ -3,6 +3,8 @@ import { screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 import { renderWithProviders } from 'utils/test-utils';
+import {walletConnectionInitialState } from '../../../store/slices/walletConnection.slice';
+import {profileInitialState} from '../../../store/slices/profile.slice';
 import AppBar from './AppBar';
 
 jest.mock('minidenticons', () => ({
@@ -11,53 +13,29 @@ jest.mock('minidenticons', () => ({
 
 describe('App bar > logged in state', () => {
     test('should render wallet address when logged in', async () => {
-        const preloadedState = {
-            walletConnection: {
-                wallet: null,
-                walletName: null,
-                walletAddress: 'addr...1234',
-                stakeAddress: null,
-                activeWallets: ['lace', 'nami', 'yoroi'],
-                errorMessage: null,
-                errorRetry: false,
-                loading: false,
-                listeningWalletChanges: false,
-                resetWalletChanges: false,
-            },
-            profile: {
-              profile: {
-                address: 'addr1234',
-                email: 'email@gmail.com',
-                fullName: 'name',
-                companyName: 'company',
-                contactEmail: 'contactemail@gmail.com',
-                linkedin: null,
-                twitter: null,
-                website: null,
-                role: null,
-                dapp: null,
-              },
-              errorMessage: null,
-              loading: false,
-              success: true,
-              selectedUser: null,
-              allUsers: null,
-              loadingDetails: false,
-              detailsSuccess: false,
-              detailsError: null,
-              userSubscription: null,
-              loadingSubDetails: false,
-              subDetailsSuccess: false,
-              subDetailsError: null,
-              updateSuccess: false,
-              loadingHistory: false,
-              runHistory: [],
-              impersonate: false,
-              retainId: null  
-            }
-          };
 
-      renderWithProviders(<AppBar />, { preloadedState });
+      renderWithProviders(<AppBar />, { preloadedState: 
+          {walletConnection: {
+          ...walletConnectionInitialState,
+          walletAddress: 'addr...1234',
+          },
+          profile: {
+            ...profileInitialState,
+            profile: {
+              address: 'addr1234',
+              email: '',
+              fullName: '',
+              companyName: '',
+              contactEmail: '',
+              linkedin: null,
+              twitter: null,
+              website: null,
+              role: null,
+              dapp: null,
+            }
+          }
+        } 
+      });
   
       const walletAddress = await screen.findByText('addr...1234');
 
